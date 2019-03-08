4fc6661fa3e817111275bc42a3a5976d35f36055
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2019-03-08 19:47:34 +0530

    User Sign in

---
+++ app/src/main/java/com/example/dell/foodapp/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.dell.foodapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signin_btn,signup_btn;
+    EditText username_edit,password_edit;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signin_btn=(Button)findViewById(R.id.signin_button);
+        signup_btn=(Button)findViewById(R.id.signup_button);
+        username_edit=(EditText)findViewById(R.id.username_edit);
+        password_edit=(EditText)findViewById(R.id.password_edit);
+        mAuth=FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+        updateUI(currentUser);
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+        signin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
+    }
+
+    void signup(){
+        String email=username_edit.getText().toString();
+        String password=password_edit.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    void signin(){
+        String email=username_edit.getText().toString();
+        String password=password_edit.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser!=null){
+            Toast.makeText(MainActivity.this,"welcome"+currentUser.getUid().toString(),Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(MainActivity.this,"Null",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
